3e365d4028c71ac427a4878c5e665b07a1347fe3
Gabriel José dos santos Pessôa <57507429+gabrieljpessoa@users.noreply.github.com>
2021-09-07 21:54:34 -0300

    Backend commit

---
+++ Backend/src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ErroDetails.java
@@ -0,0 +1,33 @@
+package com.digital.crud.saladereuniao.saladereuniao.exception;
+
+import java.util.Date;
+
+public class ErroDetails {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+
+
+    public ErroDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+
+}
